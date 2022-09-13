f868215cf7ef2bf920fcf4d7640a09ad4b019b12
tanlnguyen-fpt <tannlde150056@fpt.edu.vn>
2022-09-13 22:36:21 +0700

    Grid Panel Demo

---
+++ MyWPFApp/GridPanelDemo.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyWPFApp
+{
+    /// <summary>
+    /// Interaction logic for GridPanelDemo.xaml
+    /// </summary>
+    public partial class GridPanelDemo : Window
+    {
+        public GridPanelDemo()
+        {
+            InitializeComponent();
+        }
+    }
+}
