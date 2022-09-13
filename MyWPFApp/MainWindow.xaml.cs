25b95c9038a6dcbe79016473e236638d8f52c721
tanlnguyen-fpt <tannlde150056@fpt.edu.vn>
2022-09-13 23:13:30 +0700

    combox demo

--- MyWPFApp/MainWindow.xaml.cs
+++ MyWPFApp/MainWindow.xaml.cs
@@ -1,17 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace MyWPFApp
 {
@@ -23,6 +11,14 @@ namespace MyWPFApp
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void cbBoxColor_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var stackPanel = (StackPanel)cbBoxColor.SelectedItem;
+            var lable = stackPanel.Children[1] as Label;
+            string color = lable.Content.ToString();
+            lbMsg.Content = "Your color is " + color;
         }
     }
 }
