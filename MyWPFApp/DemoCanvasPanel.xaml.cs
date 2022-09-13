7b21e8f4f28965b2f2b51b9df289d78adc2fe283
tanlnguyen-fpt <tannlde150056@fpt.edu.vn>
2022-09-13 22:13:24 +0700

    Add project files.

---
+++ MyWPFApp/DemoCanvasPanel.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+
+namespace MyWPFApp
+{
+    /// <summary>
+    /// Interaction logic for DemoCanvasPanel.xaml
+    /// </summary>
+    public partial class DemoCanvasPanel : Window
+    {
+        public DemoCanvasPanel()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDisplay_Click(object sender, RoutedEventArgs e)
+        {
+            string carInfo = $"Car name: {txtCarName.Text}\nColor: {txtColor.Text}\nBrand: {txtBrand.Text}";
+            MessageBox.Show(carInfo, "Car detail");
+        }
+    }
+}
